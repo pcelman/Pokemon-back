a98977bea34a67236e9a3bc4b28f29024bb098c3
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2023-06-22 13:48:22 -0300

    first commit

---
+++ src/controllers/getPoke.js
@@ -0,0 +1,85 @@
+const { Router } = require('express');
+const router = Router();
+const { Pokemon, Types } = require("../db");
+const axios = require("axios").default;
+const { Op } = require("sequelize")
+
+
+
+const pokeApi = async () =>{ 
+    try {
+const apiUrl = await axios.get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=40")
+const pokeUrl = apiUrl.data.results.map(e => axios.get(e.url)) 
+const pokeInfo = await axios.all(pokeUrl)
+.then (pokes =>
+    pokes.map(p => {
+        const allPoke={
+            id: p.data.id,
+            name: p.data.name,
+            hp: p.data.stats[0].base_stat,
+            attack: p.data.stats[1].base_stat,
+            defense: p.data.stats[2].base_stat,
+            speed: p.data.stats[5].base_stat,
+            height: p.data.height,
+            weight: p.data.weight,
+            types: p.data.types.map(t=>t.type),
+            image: p.data.sprites.other.home.front_default,
+        }
+        
+        return allPoke;
+        
+    }))
+    return pokeInfo
+    } catch (error) {
+        console.log(error)
+    }}
+
+
+const dataBasePoke = async() => {
+    try {
+        const pokeDb = await Pokemon.findAll({
+        include:{
+            model: Types,
+            attributes: ["name"],
+            through: {
+                attributes: [], 
+            }
+        },
+    })
+
+
+return pokeDb 
+} catch(error) {
+    console.log(error)
+}}
+
+    // const dBApiPokes = async() => {
+    //     const apiPokes = await pokeApi()
+    //     const dbPokes = await dataBasePoke()
+    //     const totalPokes =apiPokes.concat(dbPokes)
+    //     return totalPokes
+    //     }
+    const dBApiPokes = async () => {
+        try {
+          const apiPokes = await pokeApi();
+          const dbPokes = await dataBasePoke();
+          
+          // Check if both apiPokes and dbPokes are defined
+          if (apiPokes && dbPokes) {
+            const totalPokes = apiPokes.concat(dbPokes);
+            return totalPokes;
+          } else if (apiPokes) {
+            return apiPokes;
+          } else if (dbPokes) {
+            return dbPokes;
+          } else {
+            return []; // Return an empty array if both apiPokes and dbPokes are undefined
+          }
+        } catch (error) {
+          console.log(error);
+          return []; // Return an empty array in case of any error
+        }
+      };
+      
+
+module.exports = dBApiPokes
